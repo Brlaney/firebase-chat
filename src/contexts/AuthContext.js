ca911cea2baeaa8ed809ec9fd8b436a598a73ef3
brlaney42 <brlaney@outlook.com>
2021-05-29 21:55:02 -0400

    Currently wont redirect after login

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,29 +1,30 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { auth } from '../firebase';
+import React, { useContext, useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { auth } from '../firebase'
 
-const AuthContext = React.createContext();
+const AuthContext = React.createContext()
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext)
 
-export const AuthProvider = () => ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
-  const history = useHistory();
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  const history = useHistory()
 
   useEffect(() => {
-    auth.onAuthStateChanged(() => {
-      setUser(user);
-      setLoading(false);
-      history.push('/chats');
-    })
-  }, [user, history]);
+    auth.onAuthStateChanged((user) => {
+      setUser(user)
+      setLoading(false)
 
-  const value = { user };
+      if (user) history.push('/chats')
+    })
+  }, [user, history])
+
+  const value = { user }
 
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
-  );
+  )
 }
